ca9bcc24d9ee600ba28bb98ae198c72c5d1e9327
Rebeca-UXUI <rebeca.uiux@gmail.com>
2026-02-14 12:09:00 +0100

    Create Scroll-scrubbed-text.js

---
+++ Scroll-scrubbed-text.js
@@ -0,0 +1,34 @@
+<script src="https://unpkg.com/split-type"></script>
+<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.7/dist/gsap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.7/dist/ScrollTrigger.min.js"></script>
+
+<script>
+document.addEventListener("DOMContentLoaded", function () {
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  const quotes = document.querySelectorAll(".scroll-highlight");
+
+  quotes.forEach((quote) => {
+
+    const split = new SplitType(quote, { types: "chars" });
+
+    gsap.from(split.chars, {
+      scrollTrigger: {
+        trigger: quote,
+        start: "top 80%",
+        end: "top 40%",
+        scrub: true
+      },
+      opacity: 0.2,
+      stagger: {
+        each: 0.05
+      }
+    });
+
+  });
+
+});
+</script>
+
+
